a86e73eed877b56d900274abc37e636e0837a7a3
csqttnoire <alyratojo@gmail.com>
2026-09-15 20:42:29 +0300

    Patient - formulaire view

--- src/main/java/stock/gestion/cabinet/medical/App.java
+++ src/main/java/stock/gestion/cabinet/medical/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -16,6 +17,11 @@ public class App extends Application {
     private static Scene scene;
 
     @Override
+    // public void init() throws Exception {
+    //     Font regular = Font.loadFont(getClass().getResourceAsStream("/stock/gestion/cabinet/medical/assets/fonts/OpenSans/OpenSans-Regular.ttf"), 14);
+    //     System.out.println(regular != null ? "Chargée : " + regular.getName() : "ÉCHEC : Poppins-Regular introuvable");
+    // }
+
     public void start(Stage stage) throws IOException {
 
         scene = new Scene(loadFXML("patient"), 1280, 990);
