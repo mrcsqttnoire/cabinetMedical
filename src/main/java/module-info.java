85665e8a6d3df9e3e5f8a9ed3e70954d0da12137
csqttnoire <alyratojo@gmail.com>
2026-09-15 14:50:13 +0300

    Patient - views correctoin

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module stock.gestion.cabine.medical {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires org.kordamp.ikonli.fontawesome5;
 
     opens stock.gestion.cabinet.medical to javafx.fxml;
     exports stock.gestion.cabinet.medical;
--- src/main/java/stock/gestion/cabinet/medical/App.java
+++ src/main/java/stock/gestion/cabinet/medical/App.java
@@ -4,7 +4,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -17,17 +16,6 @@ public class App extends Application {
     private static Scene scene;
 
     @Override
-    public void init() throws Exception {
-        Font regular = Font.loadFont(getClass().getResourceAsStream("/stock/gestion/cabinet/medical/assets/fonts/Poppins/Poppins-Regular.ttf"), 14);
-        System.out.println(regular != null ? "Chargée : " + regular.getName() : "ÉCHEC : Poppins-Regular introuvable");
-
-        Font semiBold = Font.loadFont(getClass().getResourceAsStream("/stock/gestion/cabinet/medical/assets/fonts/Poppins/Poppins-SemiBold.ttf"), 14);
-        System.out.println(semiBold != null ? "Chargée : " + semiBold.getName() : "ÉCHEC : Poppins-SemiBold introuvable");
-
-        Font bold = Font.loadFont(getClass().getResourceAsStream("/stock/gestion/cabinet/medical/assets/fonts/Poppins/Poppins-Bold.ttf"), 14);
-        System.out.println(bold != null ? "Chargée : " + bold.getName() : "ÉCHEC : Poppins-Bold introuvable");
-    }
-
     public void start(Stage stage) throws IOException {
 
         scene = new Scene(loadFXML("patient"), 1280, 990);
